bfe849fb8df0cfb75ae9d28178a862b427aef181
SlowL0ris <yuval.friedman@tutanota.com>
2019-01-12 15:25:58 +0200

    initial import

---
+++ src/main/java/org/team2679/frc2019/Main.java
@@ -0,0 +1,16 @@
+package org.team2679.frc2019;
+
+import org.team2679.TigerEye.core.Setup;
+
+public class Main extends Setup {
+
+    @Override
+    public void preinit() {
+        // TODO write code to be executed before HAL is told the robot has started
+    }
+
+    @Override
+    public void init() {
+        // TODO write code to be executed after HAL is told the robot has started
+    }
+}
