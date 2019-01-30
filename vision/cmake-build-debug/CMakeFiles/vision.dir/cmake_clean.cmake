f53fe1f03042a81481241fcf47600e3ad58b6f6e
SlowL0ris <yuval.friedman@tutanota.com>
2019-01-30 16:37:50 +0200

    added a opencv code that will hopefully work

---
+++ vision/cmake-build-debug/CMakeFiles/vision.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vision.dir/src/main/cpp/main.cpp.obj"
+  "vision.pdb"
+  "vision.exe"
+  "vision.exe.manifest"
+  "libvision.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
