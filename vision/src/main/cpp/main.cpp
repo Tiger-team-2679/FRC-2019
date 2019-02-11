a2317de38ce4f1e3bffbaba4b5bafa3f55f06057
slowl0ris <yuval.friedman@tutanota.com>
2019-02-11 19:50:09 +0200

    fixed camera code

--- vision/src/main/cpp/main.cpp
+++ vision/src/main/cpp/main.cpp
@@ -3,21 +3,17 @@
 #include <opencv2/core/core_c.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/videoio/videoio.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
-std::string get_tegra_pipeline(int width, int height, int fps) {
-    return "nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(width) + ", height=(int)" +
-           std::to_string(height) + ", format=(string)I420, framerate=(fraction)" + std::to_string(fps) +
-           "/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
-}
+#define WIDTH 1280
+#define HEIGHT 720
+#define FPS 30
+#define CAM_SRC "nvcamerasrc  fpsRange='FPS FPS' ! video/x-raw(memory:NVMM), width=(int)WIDTH, height=(int)HEIGHT,format=(string)I420, framerate=(fraction)FPS/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink"
 
 int main() {
-    // Options
-    int WIDTH = 1920;
-    int HEIGHT = 1080;
-    int FPS = 30;
 
     // Define the gstream pipeline
-    std::string pipeline = get_tegra_pipeline(WIDTH, HEIGHT, FPS);
+    std::string pipeline = CAM_SRC;
 
     // Create OpenCV capture object, ensure it works.
     cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
@@ -25,14 +21,16 @@ int main() {
         std::cout << "Connection failed"  << std::endl;
         return -1;
     }
-	
-	std::cout << "Connection is good" << std::endl;
 
     // View video
     cv::Mat frame;
-	cap >> frame;  // Get a new frame from camera
-
-	// Display frame
-	std::cout << "Saving image" << std::endl;
-	imwrite( "./out.jpg", frame);
+    cv::namedWindow("cam",1);
+    for(;;)
+    {
+        cap >> frame; // get a new frame from camera
+        cv::imshow("cam", frame);
+        if(cv::waitKey(30) >= 0) break;
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
 }
