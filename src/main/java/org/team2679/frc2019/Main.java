59bb3dd1e586373cd8004579c74a50613815b783
slowl0ris <yuval.friedman@tutanota.com>
2019-03-05 17:10:40 +0200

    added code to start cameras

--- src/main/java/org/team2679/frc2019/Main.java
+++ src/main/java/org/team2679/frc2019/Main.java
@@ -1,5 +1,6 @@
 package org.team2679.frc2019;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import org.team2679.TigerEye.core.Setup;
 import org.team2679.TigerEye.core.Tiger;
 import org.team2679.frc2019.subsystems.Collector;
@@ -11,15 +12,17 @@ public class Main extends Setup {
 
     @Override
     public void preinit() {
+        NetworkTablesManager.init();
+        CameraServer.getInstance().startAutomaticCapture();
         Drive.INSTANCE.registerSubsystem(Tiger.get_main_notifier_50ms());
         Collector.INSTANCE.registerSubsystem(Tiger.get_main_notifier_100ms());
         Elevator.INSTANCE.registerSubsystem(Tiger.get_main_notifier_20ms());
-        SuperStructure.INSTANCE.registerSubsystem(Tiger.get_main_notifier_50ms());
+        //SuperStructure.INSTANCE.registerSubsystem(Tiger.get_main_notifier_50ms());
     }
 
     @Override
     public void init() {
         // run the super structure to control all of the subsystems
-        SuperStructure.INSTANCE.setPutCargoAtLevel(Elevator.ELEVATOR_LEVEL.HATCH_PANEL_LEVEL_3);
+        //SuperStructure.INSTANCE.setPutCargoAtLevel(Elevator.ELEVATOR_LEVEL.HATCH_PANEL_LEVEL_3);
     }
 }
